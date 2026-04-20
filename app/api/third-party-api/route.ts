d5e59614f79698d0297323e16f2e820cd6d7a304
Nick B <ngbaker99@gmail.com>
2026-04-20 11:05:12 +0100

    remove import from third-part-api route

--- app/api/third-party-api/route.ts
+++ app/api/third-party-api/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 // Optional bonus feature - Third-party API integration
 // If you have time really think about what third-party API will make this project stand out
